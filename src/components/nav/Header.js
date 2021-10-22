7166ca1ea8d06911fc15979aaccb84fc52e864a3
LIM PUI SAN <52491578+sunnyside88@users.noreply.github.com>
2021-10-22 16:40:42 +0800

    enhanced UI

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,8 +1,8 @@
 import React, {useEffect,useState} from "react";
 import Navbar from 'react-bootstrap/Navbar'
-import { Container } from "reactstrap";
+import { Container,Row,Col } from "reactstrap";
 import moment from "moment";
-import { BsFillClockFill,BsFillCalendar2MinusFill } from "react-icons/bs";
+import { BsFillClockFill,BsFillCalendar2MinusFill,BsFillDoorOpenFill } from "react-icons/bs";
 
 const Header = () => {
     const [formmattedDate,setFormattedDate] = useState("")
@@ -16,9 +16,39 @@ const Header = () => {
          setFormattedDate(date)
     },[])
     return(
-        <Navbar style={{height:"45px"}}>
-  <Container>
-    <Navbar.Brand>Hello Chin</Navbar.Brand>
+        <Navbar sticky="top" className="shadow-none bg-light" style={{height:"45px"}}>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                <li>
+                <Navbar.Text style={{margin:"10px"}}>
+                        Hi, Chin
+                    </Navbar.Text>
+                </li>
+                <li class="nav-item">
+                    <BsFillCalendar2MinusFill/>
+                    <Navbar.Text style={{margin:"10px"}}>
+                        {formmattedDate}
+                    </Navbar.Text>
+                </li>
+                <li class="nav-item">
+                    <BsFillClockFill/>
+                    <Navbar.Text  style={{margin:"10px"}}>
+                        {formattedTime}
+                    </Navbar.Text>
+                </li>
+                </ul>
+                <span class="navbar-text">
+                    <a class="nav-link" href="#"> <BsFillDoorOpenFill/> Log out</a>
+                </span>
+            </div>
+        </Navbar>
+    );
+}
+
+export default Header;
+
+/*
+<Navbar.Brand>Hello, Chin</Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end">
         <BsFillCalendar2MinusFill/>
@@ -29,11 +59,9 @@ const Header = () => {
       <Navbar.Text  style={{margin:"10px"}}>
         {formattedTime}
       </Navbar.Text>
+      <BsFillDoorOpenFill/>
+      <Navbar.Text  style={{marginLeft:"10px"}}>
+        Log Out
+      </Navbar.Text>
     </Navbar.Collapse>
-  </Container>
-</Navbar>
-    );
-}
-
-export default Header;
-
+*/
